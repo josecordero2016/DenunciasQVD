1cccb1a3f1231878915b86e533e5b3ae9fb7d251
José Cordero Bazurto <jose.cordero2016@uteq.edu.ec>
2020-09-10 02:34:32 -0500

    Detalle denuncia

---
+++ app/src/main/java/com/foodi/actDetalleDenuncia.java
@@ -0,0 +1,14 @@
+package com.foodi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actDetalleDenuncia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_detalle_denuncia);
+    }
+}
